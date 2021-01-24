c38d63741dbfe9f99bc5babf920369b34e66441b
Madhu Sivapragasam <madhuran.siva@gmail.com>
2021-01-24 03:17:21 -0500

    Changes to the calendar component

--- Components/Calendar.js
+++ Components/Calendar.js
@@ -1,147 +1,143 @@
 import React, { useState } from "react";
-import {
-	SafeAreaView,
-	StyleSheet,
-	Button,
-	Modal,
-	View,
-	Text,
-} from "react-native";
+import { SafeAreaView, StyleSheet, Button, Modal, View } from "react-native";
 import { Agenda } from "react-native-calendars";
 import uuid from "uuid-random";
 import RenderItem from "./RenderItem";
 import EventForm from "./EventForm";
+import { Ionicons } from "@expo/vector-icons";
 
 const timeToString = (time) => {
-	const date = new Date(time);
-	return date.toISOString().split("T")[0];
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
 };
 
 const Calendar = () => {
-	const [items, setItems] = useState({});
+  const [items, setItems] = useState({});
 
-	const [timestamp, setTimestamp] = useState(0);
+  const [timestamp, setTimestamp] = useState(0);
 
-	const [modalOpen, setMOdalOpen] = useState(false);
+  const [modalOpen, setMOdalOpen] = useState(false);
 
-	const currentDay = (day) => {
-		setTimestamp(day.timestamp);
-	};
+  const currentDay = (day) => {
+    setTimestamp(day.timestamp);
+  };
 
-	const loadItems = (day) => {
-		setTimeout(() => {
-			for (let i = -15; i < 85; i++) {
-				const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-				const strTime = timeToString(time);
-				if (!items[strTime]) {
-					items[strTime] = [];
-				}
-			}
-			const newItems = {};
-			Object.keys(items).forEach((key) => {
-				newItems[key] = items[key];
-			});
-			setItems(newItems);
-		}, 1000);
-	};
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
 
-	const addItem = (items, timestamp, event) => {
-		const time = timestamp;
-		var strTime = "";
-		if (time == 0) {
-			var today = new Date();
-			const date =
-				today.getFullYear() +
-				"-" +
-				("0" + (today.getMonth() + 1)).slice(-2) +
-				"-" +
-				("0" + today.getDate()).slice(-2);
+  const addItem = (items, timestamp, event) => {
+    const time = timestamp;
+    var strTime = "";
+    if (time == 0) {
+      var today = new Date();
+      const date =
+        today.getFullYear() +
+        "-" +
+        ("0" + (today.getMonth() + 1)).slice(-2) +
+        "-" +
+        ("0" + today.getDate()).slice(-2);
 
-			strTime = date;
-		} else {
-			strTime = timeToString(time);
-		}
+      strTime = date;
+    } else {
+      strTime = timeToString(time);
+    }
 
-		items[strTime].push({
-			name: event.name,
-			time: event.time,
-			index: uuid(),
-			date: strTime,
-		});
-		console.log(event.time);
-		const newItems = {};
-		Object.keys(items).forEach((key) => {
-			newItems[key] = items[key];
-		});
-		setItems(newItems);
+    items[strTime].push({
+      name: event.name,
+      time: event.time,
+      index: uuid(),
+      date: strTime,
+      description: event.description,
+    });
+    const newItems = {};
+    Object.keys(items).forEach((key) => {
+      newItems[key] = items[key];
+    });
+    setItems(newItems);
 
-		setMOdalOpen(false);
-	};
+    setMOdalOpen(false);
+  };
 
-	const delItem = (item, items) => {
-		var i;
-		for (i = 0; i < items[item.date].length; i++) {
-			if (items[item.date][i].index == item.index) {
-				items[item.date].splice(i, 1);
-			}
-		}
-		const newItems = {};
-		Object.keys(items).forEach((key) => {
-			newItems[key] = items[key];
-		});
-		setItems(newItems);
-	};
+  const delItem = (item, items) => {
+    var i;
+    for (i = 0; i < items[item.date].length; i++) {
+      if (items[item.date][i].index == item.index) {
+        items[item.date].splice(i, 1);
+      }
+    }
+    const newItems = {};
+    Object.keys(items).forEach((key) => {
+      newItems[key] = items[key];
+    });
+    setItems(newItems);
+  };
 
-	return (
-		<SafeAreaView style={styles.safe}>
-			<Modal visible={modalOpen} animationType="slide">
-				<View style={StyleSheet.modalContent}>
-					<Button
-						title="Close form"
-						onPress={() => setMOdalOpen(false)}
-						style={styles.closeButton}
-					></Button>
-					<EventForm
-						items={items}
-						timestamp={timestamp}
-						addItem={addItem}
-					></EventForm>
-				</View>
-			</Modal>
+  return (
+    <SafeAreaView style={styles.safe}>
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={StyleSheet.modalContent}>
+          <Ionicons
+            name="close"
+            onPress={() => setMOdalOpen(false)}
+            size={30}
+            style={styles.closeButton}
+          ></Ionicons>
+          <EventForm
+            items={items}
+            timestamp={timestamp}
+            addItem={addItem}
+            setModalOpen={setMOdalOpen}
+          ></EventForm>
+        </View>
+      </Modal>
 
-			<Agenda
-				items={items}
-				renderItem={(item) => (
-					<RenderItem item={item} items={items} delItem={delItem}></RenderItem>
-				)}
-				onDayPress={currentDay}
-				onDayChange={currentDay}
-				loadItemsForMonth={loadItems}
-			/>
-			<Button
-				title="Add event"
-				//onPress={() => addItem(items, timestamp)}
-				onPress={() => setMOdalOpen(true)}
-			></Button>
-		</SafeAreaView>
-	);
+      <Agenda
+        items={items}
+        renderItem={(item) => (
+          <RenderItem item={item} items={items} delItem={delItem}></RenderItem>
+        )}
+        onDayPress={currentDay}
+        onDayChange={currentDay}
+        loadItemsForMonth={loadItems}
+      />
+      <Button
+        title="Add event"
+        //onPress={() => addItem(items, timestamp)}
+        onPress={() => setMOdalOpen(true)}
+      ></Button>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	safe: {
-		flex: 1,
-	},
-	itemContainer: {
-		backgroundColor: "white",
-		margin: 5,
-		borderRadius: 15,
-		justifyContent: "center",
-		alignItems: "center",
-		flex: 1,
-	},
-	closeButton: {
-		margin: 100,
-	},
+  safe: {
+    flex: 1,
+  },
+  itemContainer: {
+    backgroundColor: "white",
+    margin: 5,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  closeButton: {
+    marginLeft: "90%",
+  },
 });
 
 export default Calendar;
--- Components/RenderItem.js
+++ Components/RenderItem.js
@@ -1,31 +1,58 @@
 import React from "react";
-import { Text, View, Button, TouchableOpacity } from "react-native";
+import { Text, View, Button, TouchableOpacity, StyleSheet } from "react-native";
 import { Card, Avatar } from "react-native-paper";
+import { Ionicons } from "@expo/vector-icons";
 
 const RenderItem = ({ item, items, delItem }) => {
-	return (
-		<TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
-			<Card>
-				<Card.Content>
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "space-between",
-							alignItems: "center",
-						}}
-					>
-						<Button
-							title="Delete"
-							onPress={() => delItem(item, items)}
-						></Button>
-						<Text>{item.name}</Text>
-						<Text>{item.time}</Text>
-						<Avatar.Text label="🏠" />
-					</View>
-				</Card.Content>
-			</Card>
-		</TouchableOpacity>
-	);
+  return (
+    <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+      <Card>
+        <Card.Content>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Ionicons
+              name="close"
+              onPress={() => delItem(item, items)}
+              size={30}
+              style={styles.close}
+            ></Ionicons>
+            <View style={styles.textView}>
+              <Text style={styles.time}>{item.time}</Text>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+            <Avatar.Text label="🏠" />
+          </View>
+        </Card.Content>
+      </Card>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  textView: {
+    width: "60%",
+    marginRight: "5%",
+    marginLeft: "5%",
+  },
+
+  time: {
+    color: "rgb(80, 80, 80)",
+  },
+  description: {
+    color: "rgb(136,136,136)",
+  },
+  name: {
+    fontSize: 20,
+  },
+  close: {
+    color: "rgb(144,144,144)",
+  },
+});
 
 export default RenderItem;
